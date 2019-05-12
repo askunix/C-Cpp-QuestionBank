39b19d5f24c96354b4f681c03e792ab309374c4f
askunix <askunix@163.com>
2019-05-12 16:30:46 +0800

    面积计算更新

--- QT/QT5开发实例/area_5-12/main.cpp
+++ QT/QT5开发实例/area_5-12/main.cpp
@@ -4,8 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    MainWindow w;   //创建一个对话框对象
+    w.show();//创建的对话框默认不可见，调用show()函数使它变为可见。
 
-    return a.exec();
+    return a.exec(); //程序进入消息循环，等待可能的输入进行响应。就是main函数将控制权交给QT
+    //，在exec()函数中，QT接收并处理用户和系统的事件并且将他们传递给适当的窗口部件。
 }
--- QT/QT5开发实例/area_5-12/mainwindow.cpp
+++ QT/QT5开发实例/area_5-12/mainwindow.cpp
@@ -30,18 +30,15 @@ void MainWindow::on_countBtn_clicked()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+{
+    bool ok_2;
+    QString tempStr_2;
+    QString valueStr_2 = ui->lineEdit->text();
+    int valueInt_2 = valueStr_2.toInt(&ok_2);
+    double area_2 = valueInt_2 * valueInt_2 * 3.14159265;
+    ui->label_2->setText(tempStr_2.setNum(area_2));
+}
 
 
 
